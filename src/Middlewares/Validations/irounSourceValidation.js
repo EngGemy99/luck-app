c19155e413df823888a8791a64476f5d0de2fcea
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-08-21 12:50:55 +0300

    edit in vaildation

--- src/Middlewares/Validations/irounSourceValidation.js
+++ src/Middlewares/Validations/irounSourceValidation.js
@@ -1,6 +1,5 @@
 import Joi from "joi";
 
 export const addIrounSource = Joi.object({
-  apiAndroid: Joi.string().min(3).trim().required(),
-  apiIos: Joi.string().min(3).trim().required(),
+  apiKey: Joi.string().min(3).trim().required(),
 });
