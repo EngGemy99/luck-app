30124db9dfbc9bde1e785b72594e8d2fc0121c55
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-08-22 14:11:17 +0300

    add point to top offers

--- models/Offers.js
+++ models/Offers.js
@@ -26,6 +26,10 @@ const offersSchema = mongoose.Schema(
       default: "topOffer",
       enum: ["topOffer", "offersWall"],
     },
+    point: {
+      type: Number,
+      default: 0,
+    },
   },
   { timestamps: true }
 );
