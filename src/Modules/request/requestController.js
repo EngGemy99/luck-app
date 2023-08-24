655fa86cb594326e7ca6df443adb45789784132e
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-08-24 15:45:25 +0300

    edit in request end point

--- src/Modules/request/requestController.js
+++ src/Modules/request/requestController.js
@@ -2,9 +2,14 @@ import { requestModel } from "../../../models/Request.js";
 import { ErrorMessage } from "../../utils/ErrorMessage.js";
 import { catchError } from "../../utils/catchAsyncError.js";
 import { ApiFeature } from "../../utils/ApiFeature.js";
+import { userModel } from "../../../models/User.js";
 
 export const withdrawalRequest = catchError(async (request, response, next) => {
   request.body.user = request.user._id;
+  const { countPoint } = request.body;
+  if (countPoint > request.user.points) {
+    return next(ErrorMessage(404, "You Do Not Have Enough Point"));
+  }
   let newRequest = await requestModel.create(request.body);
   response.status(200).json({
     message: "waiting for admin approval",
@@ -38,6 +43,14 @@ export const acceptedOrCancelRequest = catchError(
       },
       { new: true }
     );
+    //! rival point for user in rejected or accepted
+    let user = await userModel.findById(updateRequest.user._id);
+    if (user.points >= updateRequest.countPoint) {
+      user.points -= updateRequest.countPoint;
+      await user.save();
+    } else {
+      return next(ErrorMessage(404, "User Do Not Have Enough Point"));
+    }
     response.status(200).json({
       message: "request updated successfully",
       updateRequest,
