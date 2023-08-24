b07bc1b89527797458ed1106c36bf96fbaec2ccc
Ahmed Jamal <87934810+EngGemy99@users.noreply.github.com>
2023-08-24 17:47:24 +0300

    edit validation

--- src/Middlewares/Validations/offersValidation.js
+++ src/Middlewares/Validations/offersValidation.js
@@ -4,13 +4,11 @@ export const addTopOrWallOffers = Joi.object({
   title: Joi.string().min(2).max(20).trim().required(),
   description: Joi.string().min(6).required(),
   url: Joi.string()
-    .uri({
-      scheme: [/https?/],
-    })
+    .pattern(new RegExp("^(http|https)"), "i")
     .required()
     .messages({
       "string.empty": `URL cannot be an empty field`,
-      "string.uri": `URL must be a valid URI and should start with http or https`,
+      "string.pattern.base": `URL must match the pattern http(s)`,
     }),
   offerType: Joi.string()
     .valid("topOffer", "offersWall")
